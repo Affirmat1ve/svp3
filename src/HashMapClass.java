2c25029c58f3589ef6ed16afa6f982b2fa5263d6
mainPC <himka-00@mail.ru>
2021-04-02 13:27:27 +0300

    Initial commit

---
+++ src/HashMapClass.java
@@ -0,0 +1,115 @@
+
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class HashMapClass implements java.util.Map {
+
+    private HashTableClass hashMap;
+    private int cnt;
+    private boolean nullKey;
+    private Object nullKeyValue;
+
+    HashMapClass(int size) {
+        hashMap = new HashTableClass(size);
+        cnt = 0;
+        nullKey = false;
+        nullKeyValue = null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (this.cnt == 0) ? true : false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        if(key == null) {
+            if(nullKey) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (hashMap.contains(key)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public Object get(Object key) {
+        if(key == null) {
+            if(nullKey) {
+                return nullKeyValue;
+            } else {
+                return null;
+            }
+        } else {
+            if(hashMap.contains(key)) {
+                return hashMap.get(key);
+            } else {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        if(key == null) {
+            nullKey = true;
+            nullKeyValue = value;
+        } else {
+            if (hashMap.contains(key)) {
+                hashMap.put(key, value);
+            } else {
+                hashMap.put(key, value);
+                cnt++;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+
+    @Override
+    public Object remove(Object key) {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+
+    @Override
+    public void putAll(Map m) {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+
+    @Override
+    public Set keySet() {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+
+    @Override
+    public Collection values() {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        throw new UnsupportedOperationException("Этот метод не реализован!");
+    }
+}
---
+++ src/HashTableClass.java
@@ -0,0 +1,94 @@
+
+
+import java.util.ArrayList;
+
+public class HashTableClass<K, V> {
+
+    private ArrayList<LinkedListItem<K, V>> hashTable;
+
+    private class LinkedListItem<K, V> {
+        public LinkedListItem<K, V> next;
+        public LinkedListItem<K, V> prev;
+        public K key;
+        public V value;
+
+        LinkedListItem(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    HashTableClass(int size) {
+        hashTable = new ArrayList<LinkedListItem<K, V>>();
+        hashTable.ensureCapacity(size);
+        for (int i = 0; i < size; i++) {
+            hashTable.add(null);
+        }
+    }
+
+    public V put(K key, V value) {
+        LinkedListItem<K, V> node = getNode(key);
+        if (node != null) {
+            V copyValue = node.value;
+            node.value = value;
+            return copyValue;
+        }
+        node = new LinkedListItem<K, V>(key, value);
+        int index = getIndex(key);
+        if (hashTable.get(index) != null) {
+            node.next = hashTable.get(index);
+            node.next.prev = node;
+        }
+        hashTable.set(index, node);
+        return null;
+    }
+
+    public V remove(K key) {
+        LinkedListItem<K, V> node = getNode(key);
+        if (node == null) {
+            throw new NullPointerException("Элемент с таким ключом не был найден и не может быть удален");
+        }
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            int hash = getIndex(key);
+            hashTable.set(hash, node.next);
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        }
+        return node.value;
+    }
+
+    public boolean contains(K key) {
+        if (key == null) {
+            throw new NullPointerException("Ключ не поддерживает значение null");
+        }
+        LinkedListItem<K, V> node = getNode(key);
+        return node == null ? false : true;
+    }
+
+    public V get(K key) {
+        if (key == null) {
+            throw new NullPointerException("Ключ не поддерживает значение null");
+        }
+        LinkedListItem<K, V> node = getNode(key);
+        return node == null ? null : node.value;
+    }
+
+    private LinkedListItem<K, V> getNode(K key) {
+        int index = getIndex(key);
+        LinkedListItem<K, V> currItem = hashTable.get(index);
+        while (currItem != null) {
+            if (currItem.key.equals(key)) {
+                return currItem;
+            }
+            currItem = currItem.next;
+        }
+        return null;
+    }
+
+    public int getIndex(K key) {
+        return Math.abs(key.hashCode() % hashTable.size());
+    }
+}
